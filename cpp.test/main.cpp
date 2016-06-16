f27f0d5cf1615233b0ffa3889ee3cf381b400e41
jjureta <josip.jureta@gmail.com>
2016-06-15 20:41:13 -0400

    Initial Commit

---
+++ cpp.test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp.test
+//
+//  Created by Josip Jureta on 2016-06-15.
+//  Copyright © 2016 Josip Jureta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
